fd428f0caa2958ba4a604c4b0b6a080f35400078
Faithy.dev <fathiaolusegun@gmail.com>
2025-07-11 23:37:10 +0100

    Update 9-add.js

--- 9-add.js
+++ 9-add.js
@@ -2,11 +2,7 @@ function add(a, b) {
   return a + b;
 }
 
-const a = parseInt(process.argv[2], 10);
-const b = parseInt(process.argv[3], 10);
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
 
-if (isNaN(a) || isNaN(b)) {
-  console.log("Please provide two integers");
-} else {
-  console.log(add(a, b));
-}
+console.log(add(a, b));
